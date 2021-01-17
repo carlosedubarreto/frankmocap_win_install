9bf13be33d7dfee9c1f18cdf18b4da0bd33c37d6
Carlos Barreto <carlosedubarreto@gmail.com>
2021-01-17 10:31:49 -0300

    update dependencies

--- install_win_full_body.bat
+++ install_win_full_body.bat
@@ -1,7 +1,7 @@
 
 rem ####### installing files pre packed
 
-pip install whl/faster_rcnn-0.1-cp37-cp37m-win_amd64.whl
+REM pip install whl/faster_rcnn-0.1-cp37-cp37m-win_amd64.whl
 pip install whl/detectron2-0.3-cp37-cp37m-win_amd64.whl
 pip install whl/opendr-0.73-py3-none-any.whl
 
@@ -15,6 +15,14 @@ python scripts_py/download_data_body_module.py
 
 echo Installing a third-party hand detector
 python scripts_py/install_hand_detectors.py 
+
+rem installing the hand object detector
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
+set DISTUTILS_USE_SDK=1
+cd detectors\hand_object_detector\lib
+python setup.py build develop
+cd ../../..
+
 
 echo Download extra data for hand module
 python scripts_py/download_data_hand_module.py
